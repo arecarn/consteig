08af80dddff587a3a5deef17995c046cff5448ff
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-10-17 19:19:39 -0700

    constexpr matrix multiplication working

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -3,15 +3,14 @@
 
 #include "stddef.h"
 #include "array.hpp"
-#include "gcem.hpp"
 
 template< typename T, size_t C, size_t R >
 class Matrix
 {
 public:
     //TODO(mthompkins): Fix neccessity to declare add () for an empty matrix.
-    constexpr Matrix(Array< Array<T, C>, R> m) : _data(m)
-    {}
+    //constexpr Matrix(Array< Array<T, C>, R> m) : _data(m)
+    //{}
 
     constexpr T& operator()(const size_t i, const size_t j)
     {
@@ -21,9 +20,38 @@ public:
     {
         return _data[i][j];
     }
-private:
 
-    Array< Array<T, C>, R> _data;
+    template <typename U>
+    constexpr bool operator==(const Matrix<U, R, C> &rhs) const
+    {
+        for (size_t i = 0; i < R; i++)
+            for (size_t j = 0; j < C; j++)
+                if ((*this)(i, j) != rhs(i, j))
+                    return false;
+        return true;
+    }
+
+    template <typename U>
+    constexpr bool operator!=(const Matrix<U, R, C> &rhs) const
+    {
+        return !(*this == rhs);
+    }
+
+    Array< Array<T, C>, R> _data{};
 };
+
+template< typename T, size_t C, size_t R >
+constexpr Matrix<T,C,R> operator+(
+        const Matrix<T,C,R> &lhs,
+        const Matrix<T,C,R> &rhs )
+{
+    Matrix<T,C,R> result{};
+
+    for (std::size_t i = 0; i < R; ++i)
+        for (std::size_t j = 0; j < C; ++j)
+            result(i, j) = lhs(i, j) + rhs(i, j);
+
+    return result;
+}
 
 #endif // MATRIX_HPP
--- matrix/matrix.test.cpp
+++ matrix/matrix.test.cpp
@@ -3,5 +3,59 @@
 
 TEST(matrix, static)
 {
-    const Matrix<int, 2, 2> matrix {{{{3, 5}, {2, 5}}}};
+    static constexpr int s {2};
+    static constexpr Matrix<int, s, s> matrix {{{{0, 1}, {2, 3}}}};
+
+    // Check that created objects are constexpr
+    static_assert(matrix(0,0) == 0, "Not constexpr");
+    static_assert(matrix(0,1) == 1, "Not constexpr");
+    static_assert(matrix(1,0) == 2, "Not constexpr");
+    static_assert(matrix(1,1) == 3, "Not constexpr");
+
+    int m = 0;
+    for( size_t i {0}; i<s; i++ )
+    {
+        for( size_t j {0}; j<s; j++ )
+        {
+            ASSERT_EQ(m, matrix(i,j));
+            m++;
+        }
+    }
+}
+
+TEST(matrix, equality)
+{
+    static constexpr int s {2};
+    static constexpr Matrix<int, s, s> mat1 {{{{0, 1}, {2, 3}}}};
+    static constexpr Matrix<int, s, s> mat2 {{{{0, 1}, {2, 3}}}};
+    static constexpr Matrix<int, s, s> mat3 {{{{3, 2}, {1, 0}}}};
+
+    // Check that created objects are constexpr
+    static_assert(mat1==mat2, "Not constexpr");
+    static_assert(mat2!=mat3, "Not constexpr");
+
+    ASSERT_TRUE(mat1==mat2);
+    ASSERT_FALSE(mat2==mat3);
+
+    ASSERT_TRUE(mat1!=mat3);
+    ASSERT_FALSE(mat1!=mat2);
+}
+
+TEST(matrix, addition)
+{
+    static constexpr int s {2};
+    static constexpr Matrix<int, s, s> mat1 {{{{3, 4}, {5, 6}}}};
+    static constexpr Matrix<int, s, s> mat2 {{{{1, 3}, {2, 5}}}};
+
+    static constexpr Matrix<int, s, s> answer {{{{4, 7}, {7, 11}}}};
+    static constexpr Matrix<int, s, s> test {mat1 + mat2};
+
+
+    for( size_t i {0}; i<s; i++ )
+    {
+        for( size_t j {0}; j<s; j++ )
+        {
+            ASSERT_EQ( test(i,j),answer(i,j) );
+        }
+    }
 }
