f98df43d3209b88980b74bb429ff381394f32ca9
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-10-24 03:06:57 -0700

    temporary test main file

---
+++ solver/main.cpp
@@ -0,0 +1,42 @@
+#include "qr_decomp.hpp"
+#include "gcem.hpp"
+#include <iostream>
+
+int main()
+{
+    int y = 0;
+
+    static constexpr int x {3};
+
+    Matrix<float, x, x> mat
+    {{{ {1.0F, 1.0F, 0.0F}, {1.0F, 0.0F , 1.0F}, {0.0F, 0.0F, 1.0F} }}};
+
+    QrMatrix<float, x> qr {qrDecomp(mat)};
+
+    static constexpr Matrix<float, x, x> qAnswer
+    {{{
+        {1.0F/gcem::sqrt(2),  1.0F/gcem::sqrt(6), -1.0F/gcem::sqrt(3)},
+        {1.0F/gcem::sqrt(2), -1.0F/gcem::sqrt(6),  1.0F/gcem::sqrt(3)},
+        {0.0F,                2.0F/gcem::sqrt(6),  1.0F/gcem::sqrt(3)}
+    }}};
+
+    for(int i {0}; i<x; i++)
+    {
+        for(int j {0}; j<x; j++)
+        {
+            std::cout << qr._q(i,j) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\n";
+    for(int i {0}; i<x; i++)
+    {
+        for(int j {0}; j<x; j++)
+        {
+            std::cout << qAnswer(i,j) << " ";
+        }
+        std::cout << "\n";
+    }
+
+}
