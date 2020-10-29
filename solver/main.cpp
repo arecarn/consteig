b7298adeed98d77ba93d9a358dcc7331c1cfad78
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-10-28 23:30:11 -0700

    Realized I was pulling row not column vectors

--- solver/main.cpp
+++ solver/main.cpp
@@ -8,10 +8,13 @@ int main()
 
     static constexpr int x {3};
 
-    static constexpr Matrix<float, x, x> mat
-    {{{ {1.0F, 1.0F, 0.0F}, {1.0F, 0.0F , 1.0F}, {0.0F, 1.0F, 1.0F} }}};
+    //static constexpr Matrix<float, x, x> mat
+    //{{{ {1.0F, 1.0F, 0.0F}, {1.0F, 0.0F , 1.0F}, {0.0F, 1.0F, 1.0F} }}};
 
-    static constexpr QrMatrix<float, x> qr {qrDecomp(mat)};
+    Matrix<float, x, x> mat
+    {{{ {1.0F, 6.0F, 62.0F}, {1.0F, 5.0F , 22.0F}, {-1.0F, 3.0F, 11.0F} }}};
+
+    QrMatrix<float, x> qr {qrDecomp(mat)};
 
     static constexpr Matrix<float, x, x> qAnswer
     {{{
@@ -37,15 +40,15 @@ int main()
         std::cout << "\n";
     }
 
-    std::cout << "\n";
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << qAnswer(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
+    //std::cout << "\n";
+    //for(int i {0}; i<x; i++)
+    //{
+    //    for(int j {0}; j<x; j++)
+    //    {
+    //        std::cout << qAnswer(i,j) << " ";
+    //    }
+    //    std::cout << "\n";
+    //}
 
     std::cout << "\nR Matrix:\n";
     for(int i {0}; i<x; i++)
@@ -57,14 +60,14 @@ int main()
         std::cout << "\n";
     }
 
-    std::cout << "\n";
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << rAnswer(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
+    //std::cout << "\n";
+    //for(int i {0}; i<x; i++)
+    //{
+    //    for(int j {0}; j<x; j++)
+    //    {
+    //        std::cout << rAnswer(i,j) << " ";
+    //    }
+    //    std::cout << "\n";
+    //}
 
 }
