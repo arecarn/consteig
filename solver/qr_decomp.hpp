c4bab0ccc48abbce906abf833b8c92492dfeffcd
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-08 01:51:54 -0800

    CGS -> MGS

--- solver/qr_decomp.hpp
+++ solver/qr_decomp.hpp
@@ -25,26 +25,29 @@ constexpr QrMatrix<T, C> qrDecomp( const Matrix<T,R,C> x )
     static_assert( R==C, "QR decomposition must be a square matrix");
 
     QrMatrix<T,C> result {static_cast<T>(0)};
+    Matrix<T,C,C> &q {result._q};
+    Matrix<T,C,C> &r {result._r};
 
-    // Solve q by modified gram-schmidt process
-    // (https://www.math.ucla.edu/~yanovsky/Teaching/Math151B/handouts/GramSchmidt.pdf)
+    // Solve q & R by modified gram-schmidt process
+    // https://www.math.ucla.edu/~yanovsky/Teaching/Math151B/handouts/GramSchmidt.pdf
+    // https://www.math.ucla.edu/~yanovsky/Teaching/Math151B/handouts/gsqr.m
     // https://www.math.uci.edu/~ttrogdon/105A/html/Lecture23.html
 
     for(size_t i {0}; i<C; i++)
     {
-        result._q.setCol(x.col(i), i);
+        q.setCol(x.col(i), i);
 
         for(size_t j {0}; j<i; j++)
         {
-            result._r(j,i) = (transpose(result._q.col(j)) * x.col(i))(0,0);
-            Matrix<T,C,1> step {result._q.col(i) - (result._r(j,i)*result._q.col(j))};
-            result._q.setCol(step, i);
+            r(j,i) = dot(transpose(q.col(j)), transpose(q.col(i)));
+            Matrix<T,C,1> step {q.col(i) - (r(j,i)*q.col(j))};
+            q.setCol(step, i);
         }
-        result._r(i,i) = normE(result._q.col(i));
+        r(i,i) = normE(q.col(i));
 
         // This is to handle a divide by zero below
-        if(result._r(i,i) != static_cast<T>(0))
-            result._q.setCol( (static_cast<T>(1)/result._r(i,i)) * result._q.col(i), i  );
+        if(r(i,i) != static_cast<T>(0))
+            q.setCol( (static_cast<T>(1)/r(i,i)) * q.col(i), i  );
     }
 
     return result;
