5879bf9b0ff0abc20e8f5ede5ca767d5568929be
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-08 01:51:54 -0800

    CGS -> MGS

--- solver/qr_decomp.test.cpp
+++ solver/qr_decomp.test.cpp
@@ -127,19 +127,19 @@ TEST(qr_decomp, static_constexpr_random)
 
 TEST(qr_decomp, static_constexpr_eigenValues)
 {
-    //static constexpr size_t x {4};
+    static constexpr size_t x {4};
 
-    //static constexpr Matrix<float,x,x> mat
-    //{{{
-    //    { -4.4529e-01,  4.9063e+00, -8.7871e-01,  6.3036e+00},
-    //    { -6.3941e+00,  1.3354e+01,  1.6668e+00,  1.1945e+01},
-    //    {  3.6842e+00, -6.6617e+00, -6.0021e-02, -7.0043e+00},
-    //    {  3.1209e+00, -5.2052e+00, -1.4130e+00, -2.8484e+00}
-    //}}};
+    static constexpr Matrix<float,x,x> mat
+    {{{
+        { -4.4529e-01,  4.9063e+00, -8.7871e-01,  6.3036e+00},
+        { -6.3941e+00,  1.3354e+01,  1.6668e+00,  1.1945e+01},
+        {  3.6842e+00, -6.6617e+00, -6.0021e-02, -7.0043e+00},
+        {  3.1209e+00, -5.2052e+00, -1.4130e+00, -2.8484e+00}
+    }}};
 
-    //static constexpr auto eigenValueTest {eigenvalues(mat)};
+    static constexpr auto eigenValueTest {eigenvalues(mat)};
 
-    //static constexpr bool checkEigen = checkEigenValues<float,x,x>(mat,eigenValueTest);
-    ////static_assert(checkEigen==true, MSG);
+    static constexpr bool checkEigen = checkEigenValues<float,x,x>(mat,eigenValueTest);
+    //static_assert(checkEigen==true, MSG);
     //ASSERT_TRUE( checkEigen );
 }
