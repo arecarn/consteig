2c7763024219ade25e25a928a0ee89b235d47f75
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-14 18:57:32 -0800

    hessenberg almost there

--- solver/main.cpp
+++ solver/main.cpp
@@ -148,4 +148,15 @@ int main()
     bool equal = checkEigenValues<float,4,4>(matlab,eigenValueTest);
     std::cout << equal << "\n";
 
+    Matrix<float,4,4> extractionTest
+    {{{
+        { -4.4529e-01,  4.9063e+00, -8.7871e-01,  6.3036e+00},
+        { -6.3941e+00,  1.3354e+01,  1.6668e+00,  1.1945e+01},
+        {  3.6842e+00, -6.6617e+00, -6.0021e-02, -7.0043e+00},
+        {  3.1209e+00, -5.2052e+00, -1.4130e+00, -2.8484e+00}
+    }}};
+
+    auto extract {extractionTest.col<1,3>(0)};
+    auto testHess {hess(extractionTest)};
+
 }
