15b0bc69d546c529f5c9488acc96e0d9ee166422
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-24 00:21:11 -0800

    Cleaning up files. Re-organizing

---
+++ matrix/decompositions/hessenberg.hpp
@@ -0,0 +1,95 @@
+#ifndef HESSENBERG_HPP
+#define HESSENBERG_HPP
+
+#include "stddef.h"
+#include "matrix.hpp"
+
+/*
+ * This warrants an explantation.
+ */
+
+///////////// TYPES /////////////
+template<typename T, size_t S>
+struct PHMatrix
+{
+    Matrix<T,S,S> _p;
+    Matrix<T,S,S> _h;
+
+    constexpr void operator=(const PHMatrix<T, S> &rhs)
+    {
+        for( size_t i{0}; i<S; i++ )
+            for( size_t j{0}; j<S; j++ )
+            {
+                (*this._p)(i,j) = rhs._p(i,j);
+                (*this._h)(i,j) = rhs._h(i,j);
+            }
+    }
+};
+
+///////////// FUNCTION DECLARATIONS /////////////
+template<typename T, size_t R, size_t C, size_t L=R>
+constexpr PHMatrix<T,R> hess(Matrix<T,R,C> a);
+
+
+///////////// IMPLEMENTATIONS /////////////
+template<typename T, size_t R, size_t C, size_t L=R>
+struct hess_impl
+{
+    static constexpr PHMatrix<T,R> _(Matrix<T,R,C> a)
+    {
+        static_assert( R==C, "Hessenberg reduction expects a square matrix");
+
+        constexpr size_t size {R};
+        constexpr size_t houseSize {L};
+        constexpr size_t end {R-1};
+
+        Matrix<T,L,L> subA
+        {
+            a.template sub<
+                R-houseSize,
+                R-houseSize,
+                end,
+                end>()
+        };
+        Matrix<T,L,L> m {house(subA)};
+
+        Matrix<T,size,size> p {eye<T,R>()};
+        p.template setSub<
+            R-houseSize+1,
+            R-houseSize+1,
+            end,
+            end>(m.template sub<1,1,houseSize-1,houseSize-1>());
+
+        PHMatrix<T,R> out = hess<T,R,R,houseSize-1>(p*a*p);
+
+        Matrix<T,size,size> pRtn { (houseSize>3) ? p*out._p : p };
+
+        return
+        {
+            ._p = pRtn,
+            ._h = out._h
+        };
+    }
+};
+
+template<typename T, size_t R, size_t C>
+struct hess_impl<T, R, C, 2>
+{
+    static constexpr PHMatrix<T,R> _(Matrix<T,R,C> a)
+    {
+        return
+        {
+            ._p = {0},
+            ._h = a
+        };
+    }
+};
+
+// https://www.fluentcpp.com/2017/08/11/how-to-do-partial-template-specialization-in-c/
+template<typename T, size_t R, size_t C, size_t L = R>
+constexpr PHMatrix<T,R> hess(Matrix<T,R,C> a)
+{
+    return hess_impl<T,R,C,L>::_(a);
+};
+
+#endif
