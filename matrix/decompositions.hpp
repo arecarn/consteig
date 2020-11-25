a2df247afd44ed93121f8cd4384cebff619cfd7d
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-24 23:23:23 -0800

    Added some files

---
+++ matrix/decompositions.hpp
@@ -0,0 +1,11 @@
+#ifndef DECOMPOSITIONS_HPP
+#define DECOMPOSITIONS_HPP
+
+namespace constmat
+{
+#include  "decompositions/house.hpp"
+#include  "decompositions/hessenberg.hpp"
+#include  "decompositions/qr.hpp"
+}
+
+#endif
