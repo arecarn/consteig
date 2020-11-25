05dfa67ac29a88615f405b0ea176b01a45f8a6c3
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-25 00:37:39 -0800

    Playing with file locations

---
+++ constmat.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTMAT_HPP
+#define CONSTMAT_HPP
+
+namespace constmat
+{
+#include "matrix/matrix.hpp"
+#include "matrix/decompositions.hpp"
+#include "matrix/operations.hpp"
+}
+
+#endif
