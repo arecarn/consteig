b387c5d5b9c1d2d32bfaa495fe0ecbd49c6d5384
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-27 11:03:51 -0800

    Cleaned up matrix example

--- examples/matrix.cpp
+++ examples/matrix.cpp
@@ -1,106 +1,26 @@
-#include <iostream>
-
-#include "stddef.h"
-#include "../constmat.hpp"
-
-
-using namespace constmat;
+#include "example_helper.hpp"
 
 int main()
 {
     static constexpr int x {3};
 
-    static constexpr constmat::Matrix<int, 1, x> mat0
-    {{{{5, -4, 2}}}};
-    static constexpr Matrix<int, 1, x> mat1
-    {{{{-1, 2 , 3}}}};
-    static constexpr Matrix<int, 1, x> mat2
-    {{{{-2, 1, 0}}}};
-
-    Matrix<int,x,x> mat {};
-    mat.setRow(mat0,0);
-    mat.setRow(mat1,1);
-    mat.setRow(mat2,2);
-
-    constexpr Matrix<int, x, x> answer
+    // Create a 3x3 matrix
+    static constexpr constmat::Matrix<int, x, x> mat1
     {{{{5, -4, 2}, {-1, 2 , 3}, {-2, 1, 0}}}};
 
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << mat(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
+    // Create a matrix which is the transpose
+    static constexpr constmat::Matrix<int, x, x> mat2 {transpose(mat1)};
 
-    std::cout << "\n";
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << answer(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
+    // Multiply the two
+    static constexpr constmat::Matrix<int, x, x> mat3 {mat1*mat2};
 
-    Matrix<float, 2, 2> matTest
-    {{{{1.0F, -7.0F}, {-2.0F, 3.0F}}}};
+    // Multiply by a scalar
+    static constexpr constmat::Matrix<int, x, x> mat4 {-3*mat2};
+    //TODO(mthompkins): Investigate this
+    //static constexpr constmat::Matrix<float, x, x> mat4 {3.4*static_cast<constmat::Matrix<float, x, x>>(mat2)};
 
-    static constexpr Matrix<float, x, x> three
-    {{{{5, -4, 2}, {-1, 2 , 3}, {-2, 1, 0}}}};
-
-    static constexpr float n3 {det(three)};
-
-    std::cout << n3 << "\n";
-
-    Matrix<int, x, x> colTest
-    {{{{5, -4, 2}, {-1, 2 , 3}, {-2, 1, 0}}}};
-
-    auto colExtract = colTest.col<1,2>(0);
-
-    for(int i {0}; i<2; i++)
-    {
-        std::cout << colExtract(i,0) << " ";
-    }
-
-    static constexpr Matrix<int, 1, 3> rowTest1
-    {{{{99, -7, 8}}}};
-
-    static constexpr Matrix<int, 1, 2> rowTest2
-    {{{{-1, 257}}}};
-
-    std::cout << "\n\nBefore:\n";
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << colTest(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
-
-    //colTest.setRow( rowTest1, 0 );
-
-    std::cout << "\n\nAfter 1:\n";
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << colTest(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
-
-    colTest.setRow<1,2>( rowTest2, 1 );
-
-    std::cout << "\n\nAfter 2:\n";
-    for(int i {0}; i<x; i++)
-    {
-        for(int j {0}; j<x; j++)
-        {
-            std::cout << colTest(i,j) << " ";
-        }
-        std::cout << "\n";
-    }
+    printMat("Mat1", mat1);
+    printMat("Mat2", mat2);
+    printMat("Mat3", mat3);
+    printMat("Mat4", mat4);
 }
