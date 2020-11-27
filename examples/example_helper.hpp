0f424aff35bf7ca138537ce5a8dae1e53a78f973
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-27 13:19:10 -0800

    forgot to add this

---
+++ examples/example_helper.hpp
@@ -0,0 +1,23 @@
+#ifndef EXAMPLER_HELPER_HPP
+#define EXAMPLER_HELPER_HPP
+
+#include <iostream>
+#include <string>
+#include "stddef.h"
+#include "../constmat.hpp"
+
+template<typename T, size_t R, size_t C>
+void printMat( const std::string name, const constmat::Matrix<T,R,C> &mat)
+{
+    std::cout << "\n" << name << "\n";
+    for(int i {0}; i<R; i++)
+    {
+        for(int j {0}; j<C; j++)
+        {
+            std::cout << mat(i,j) << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+#endif
