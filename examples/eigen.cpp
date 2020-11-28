43a413bc91ac364cdd64dd329cc9cadd6e8a922b
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-28 11:37:57 -0800

    eigs has infinite loop

--- examples/eigen.cpp
+++ examples/eigen.cpp
@@ -25,7 +25,7 @@ int main()
 {
     static constexpr size_t s {4};
 
-    static constexpr constmat::Matrix<float,s,s> mat
+    /*static constexpr*/ constmat::Matrix<float,s,s> mat
     {{{
     {-0.44529, 4.9063, -0.87871, 6.3036},
     {-6.3941, 13.354, 1.6668, 11.945},
@@ -33,8 +33,9 @@ int main()
     {3.1209, -5.2052, -1.413, -2.8484},
     }}};
 
-    static constexpr constmat::Matrix<float,s,s> testEig {consteig::eig(mat)};
+    /*static constexpr*/ constmat::Matrix<float,s,s> testEig {consteig::eig(mat)};
 
+    printMat("testEig", testEig);
 }
 
 // T = [ -4.4529e-01  4.9063e+00 -8.7871e-01  6.3036e+00; -6.3941e+00  1.3354e+01  1.6668e+00  1.1945e+01; 3.6842e+00 -6.6617e+00 -6.0021e-02 -7.0043e+00; 3.1209e+00 -5.2052e+00 -1.4130e+00 -2.8484e+00; ];
