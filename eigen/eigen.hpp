2ad462b22c5885e241844c8b1a36904842481ad3
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-29 20:22:44 -0800

    getting closer....

--- eigen/eigen.hpp
+++ eigen/eigen.hpp
@@ -6,6 +6,9 @@
 namespace consteig
 {
 
+template<typename T, size_t S, size_t L=S>
+constexpr constmat::Matrix<T,S,S> eig( constmat::Matrix<T,S,S> a );
+
 template<typename T>
 constexpr T wilkinsonShift(const T a, const T b, const T c)
 {
@@ -14,40 +17,55 @@ constexpr T wilkinsonShift(const T a, const T b, const T c)
     if( delta == static_cast<T>(0) )
         delta = static_cast<T>(1);
 
-    return c - (gcem::sgn(delta)*gcem::pow(b,2)/
-        (gcem::abs(delta) + gcem::sqrt(gcem::pow(delta,2)+gcem::pow(delta,2))));
+    return (c - ((gcem::sgn(delta)*gcem::pow(b,2))/
+        (gcem::abs(delta) + gcem::sqrt( gcem::pow(delta,2)+gcem::pow(b,2) ))));
 }
 
 // http://pi.math.cornell.edu/~web6140/TopTenAlgorithms/QRalgorithm.html
 template<typename T, size_t S, size_t L=S>
+struct eig_impl
+{
+    static constexpr constmat::Matrix<T,S,S> _( constmat::Matrix<T,S,S> a )
+    {
+        static_assert( constmat::is_float<T>(), "eigs expects floating point");
+
+        constexpr size_t size {S};
+        constexpr size_t end {S-1};
+
+        constmat::PHMatrix<T,S> hessTemp {constmat::hess(a)};
+        a = hessTemp._h;
+
+        while( normE(a.template sub<L-1,L-2,L-1,L-2>()) > 1e-10 )
+        {
+            T mu { wilkinsonShift( a(L-2,L-2), a(L-1,L-1), a(L-2,L-1) ) };
+
+            constmat::Matrix<T,S,S> tempEye { (mu*constmat::eye<T,S>()) };
+            constmat::QRMatrix<T,S> tempQr { constmat::qr( a-tempEye ) };
+
+            a = (tempQr._r*tempQr._q) + tempEye;
+        }
+
+        a = eig<T,S,L-1>(a);
+
+        return a;
+    }
+};
+
+template<typename T, size_t S>
+struct eig_impl<T,S,1>
+{
+    static constexpr constmat::Matrix<T,S,S> _( constmat::Matrix<T,S,S> a )
+    {
+        return a;
+    }
+};
+
+template<typename T, size_t S, size_t L=S>
 constexpr constmat::Matrix<T,S,S> eig( constmat::Matrix<T,S,S> a )
 {
-    static_assert( constmat::is_float<T>(), "eigs expects floating point");
-
-    constexpr size_t size {S};
-    constexpr size_t end {S-1};
-
-    constmat::PHMatrix<T,S> hessTemp {constmat::hess(a)};
-    a = hessTemp._h;
-
-    while( normE(a.template sub<0,0,0,0>()) > 1e-10 )
-    {
-        T test {normE(a.template sub<0,0,0,0>())};
-        T mu { wilkinsonShift( a(L-2,L-2), a(L-1,L-1), a(L-2,L-1) ) };
-        constmat::Matrix<T,S,S> tempEye { (mu*constmat::eye<T,S>()) };
-        constmat::QRMatrix<T,S> tempQr { constmat::qr( a-tempEye ) };
-        a = (tempQr._r*tempQr._q) + tempEye;
-        int x {0};
-    }
-
-    return a;
-}
-
-template<typename T>
-constexpr constmat::Matrix<T,1,1> eig( const constmat::Matrix<T,1,1> a )
-{
-    return a;
-}
+    static_assert( constmat::is_float<T>(), "eig reduction  expects floating point");
+    return eig_impl<T,S,L>::_(a);
+};
 
 //template<typename T, size_t R, size_t C>
 //constexpr Matrix<T, R, 1> eigvals( const Matrix<T,R,C> x )
