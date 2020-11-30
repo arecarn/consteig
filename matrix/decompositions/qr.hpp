dbbad9f843bcfa6ecdc3b38dbb93969546a98370
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-30 00:23:23 -0800

    Modified MGS to be consistent with commonly found definition. TBD if it is correct

--- matrix/decompositions/qr.hpp
+++ matrix/decompositions/qr.hpp
@@ -17,35 +17,39 @@ struct QRMatrix
 };
 
 template<typename T, size_t R, size_t C>
-constexpr QRMatrix<T, C> qr( const Matrix<T,R,C> x )
+constexpr QRMatrix<T, C> qr( const Matrix<T,R,C> a )
 {
     //TODO(mthompkins): Remove this necessity
     static_assert( R==C, "QR decomposition must be a square matrix");
 
-    QRMatrix<T,C> result {static_cast<T>(0)};
-    Matrix<T,C,C> &q {result._q};
-    Matrix<T,C,C> &r {result._r};
+    QRMatrix<T,R> result {static_cast<T>(0)};
+    Matrix<T,R,R> &q {result._q};
+    Matrix<T,R,R> &r {result._r};
 
     // Solve Q & R by modified gram-schmidt process
     // https://www.math.ucla.edu/~yanovsky/Teaching/Math151B/handouts/GramSchmidt.pdf
     // https://www.math.ucla.edu/~yanovsky/Teaching/Math151B/handouts/gsqr.m
     // https://www.math.uci.edu/~ttrogdon/105A/html/Lecture23.html
+    
+    Matrix<T,R,R> v {a};
 
-    for(size_t i {0}; i<C; i++)
+    for(size_t i {0}; i<R; i++)
     {
-        q.setCol(x.col(i), i);
+        r(i,i) = normE(v.col(i));
 
-        for(size_t j {0}; j<i; j++)
+        //TODO(mthompkins): Ensure that this is the right thing to be doing....
+        if( r(i,i) != static_cast<T>(0))
         {
-            r(j,i) = dot(transpose(q.col(j)), transpose(q.col(i)));
-            Matrix<T,C,1> step {q.col(i) - (r(j,i)*q.col(j))};
-            q.setCol(step, i);
+            Matrix<T,R,1> setQCol { (1/r(i,i)) * v.col(i)};
+            q.setCol(setQCol, i);
         }
-        r(i,i) = normE(q.col(i));
 
-        // This is to handle a divide by zero below
-        if(r(i,i) != static_cast<T>(0))
-            q.setCol( (static_cast<T>(1)/r(i,i)) * q.col(i), i  );
+        for(size_t j {i+1}; j<R; j++)
+        {
+            r(i,j) = dot( transpose(q.col(i)), transpose(v.col(j)) );
+            Matrix<T,R,1> setVCol { v.col(j) - (r(i,j)*q.col(i)) };
+            v.setCol(setVCol, j);
+        }
     }
 
     return result;
