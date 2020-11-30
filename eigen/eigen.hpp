0ec6de2247d097f88af64963c00bc46366a61322
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-11-30 09:09:04 -0800

    Eigen works but looks to have some numerical errors

--- eigen/eigen.hpp
+++ eigen/eigen.hpp
@@ -6,7 +6,7 @@
 namespace consteig
 {
 
-template<typename T, size_t S, size_t L=S>
+template<typename T, size_t S>
 constexpr constmat::Matrix<T,S,S> eig( constmat::Matrix<T,S,S> a );
 
 template<typename T>
@@ -22,7 +22,7 @@ constexpr T wilkinsonShift(const T a, const T b, const T c)
 }
 
 // http://pi.math.cornell.edu/~web6140/TopTenAlgorithms/QRalgorithm.html
-template<typename T, size_t S, size_t L=S>
+template<typename T, size_t S>
 struct eig_impl
 {
     static constexpr constmat::Matrix<T,S,S> _( constmat::Matrix<T,S,S> a )
@@ -30,45 +30,46 @@ struct eig_impl
         constexpr size_t size {S};
         constexpr size_t end {S-1};
 
-        constmat::PHMatrix<T,L> hessTemp {constmat::hess(a)};
+        constmat::PHMatrix<T,S> hessTemp {constmat::hess(a)};
         a = hessTemp._h;
 
-        while( normE(a.template sub<L-1,L-2,L-1,L-2>()) > 1e-10 )
+        while( normE(a.template sub<S-1,S-2,S-1,S-2>()) > 1e-10 )
         {
-            T mu { wilkinsonShift( a(L-2,L-2), a(L-1,L-1), a(L-2,L-1) ) };
+            T mu { wilkinsonShift( a(S-2,S-2), a(S-1,S-1), a(S-2,S-1) ) };
 
-            constmat::Matrix<T,L,L> tempEye { (mu*constmat::eye<T,L>()) };
-            constmat::QRMatrix<T,L> tempQr { constmat::qr( a-tempEye ) };
+            constmat::Matrix<T,S,S> tempEye { (mu*constmat::eye<T,S>()) };
+            constmat::QRMatrix<T,S> tempQr { constmat::qr( a-tempEye ) };
 
             a = (tempQr._r*tempQr._q) + tempEye;
         }
 
-        constmat::Matrix<T,S-1,L-1> subA { a.template sub<0,0,S-2,S-2>() };
+        constmat::Matrix<T,S-1,S-1> subA { a.template sub<0,0,S-2,S-2>() };
 
-        constmat::Matrix<T,S-1,L-1> out = eig<T,S-1,L-1>(subA);
+        constmat::Matrix<T,S-1,S-1> out = eig<T,S-1>(subA);
 
-        auto i {constmat::eye<T,L>()};
+        auto i {constmat::eye<T,S>()};
         i.template setSub<1,1,end,end>(out);
-        i(0,0) = a(L-1,L-1);
+        i(0,0) = a(S-1,S-1);
 
         return i;
     }
 };
 
 template<typename T>
-struct eig_impl<T,1,1>
+struct eig_impl<T,1>
 {
     static constexpr constmat::Matrix<T,1,1> _( constmat::Matrix<T,1,1> a )
     {
+        static_assert( constmat::is_float<T>(), "eig reduction expects floating point");
         return a;
     }
 };
 
-template<typename T, size_t S, size_t L=S>
+template<typename T, size_t S>
 constexpr constmat::Matrix<T,S,S> eig( constmat::Matrix<T,S,S> a )
 {
     static_assert( constmat::is_float<T>(), "eig reduction expects floating point");
-    return eig_impl<T,S,L>::_(a);
+    return eig_impl<T,S>::_(a);
 };
 
 template<typename T, size_t S>
