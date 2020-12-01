934bdf18c4391bcd5a12175fd809ca1eceb59d67
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-12-01 15:27:59 -0800

    Removing master branch content for now

--- consteig.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef CONSTEIG_HPP
-#define CONSTEIG_HPP
-
-#include "eigen/eigen.hpp"
-
-#endif //CONSTEIG_HPP
--- matrix/decompositions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DECOMPOSITIONS_HPP
-#define DECOMPOSITIONS_HPP
-
-#include  "decompositions/decompositions_helper.hpp"
-#include  "decompositions/householder.hpp"
-#include  "decompositions/hessenberg.hpp"
-#include  "decompositions/qr.hpp"
-
-#endif
